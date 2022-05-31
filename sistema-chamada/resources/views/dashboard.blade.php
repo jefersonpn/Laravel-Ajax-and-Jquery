b61182908e3e3c986e588ee1babbabea4c267a65
Jeferson Nascimento <67881060+jefersonpn@users.noreply.github.com>
2022-05-31 00:02:22 -0400

    How can you give permission to user or admin see

--- sistema-chamada/resources/views/dashboard.blade.php
+++ sistema-chamada/resources/views/dashboard.blade.php
@@ -9,6 +9,12 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
+                    @can('user')
+                        <h4>Dados do Usuário!</h4>
+                    @elsecan('admin')
+                        <h4>Somente do admin pode ver isso.</h4>
+                    @endcan
+                    
                     You're logged in!
                 </div>
             </div>
