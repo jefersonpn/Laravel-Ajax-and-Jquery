8b303466ed5b2427f6a3ee9a7b679c92eb5f1a73
Jeferson Nascimento <67881060+jefersonpn@users.noreply.github.com>
2022-05-30 21:14:12 -0400

    Changing ROUTE to login page

--- sistema-chamada/routes/web.php
+++ sistema-chamada/routes/web.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('auth.login');
-});
-
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
