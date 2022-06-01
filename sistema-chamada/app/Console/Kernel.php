95c0c5ccbd26ddee73b668f61bdf60b5f15a37ac
Jeferson Nascimento <67881060+jefersonpn@users.noreply.github.com>
2022-06-01 19:36:32 -0400

    Starting the Project using Laravel | Argon

--- sistema-chamada/app/Console/Kernel.php
+++ sistema-chamada/app/Console/Kernel.php
@@ -2,11 +2,21 @@
 
 namespace App\Console;
 
+use App\Console\Commands\DeleteOldUsers;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
+    /**
+     * The Artisan commands provided by your application.
+     *
+     * @var array
+     */
+    protected $commands = [
+        //
+    ];
+
     /**
      * Define the application's command schedule.
      *
@@ -15,7 +25,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        // * * * * * cd /path-to-your-project && php artisan schedule:run >> /dev/null 2>&1
+        $schedule->command(DeleteOldUsers::class)->daily();
     }
 
     /**
--- sistema-chamada/resources/views/dashboard.blade.php
+++ sistema-chamada/resources/views/dashboard.blade.php
@@ -4,20 +4,41 @@
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
-
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
+                    <div class="container-fluid">
+                        <div class="row min-vh-100 flex-column flex-md-row">
+                            <aside class="col-12 col-md2 p-0 bg-light flex-shrink-1">
+                                <nav class="navbar navbard-expand navbar-light bg-light flex-md-column flex-row align-items-start py-2">
+                                    <div class="collapse navbar-collapse">
+                                        <ul class="flex-md-column flex-row navbar-nav w-100 justify-content-between">
+                                            <li class="nav-item">
+                                                <a href="#" class="nav-link pl-0 text-nowrap">Link1</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a href="#" class="nav-link pl-0 text-nowrap">Link2</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a href="#" class="nav-link pl-0 text-nowrap">Link3</a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </nav>
+                            </aside>
+                        </div>
+                    </div>
                     @can('user')
                         <h4>Dados do Usuário!</h4>
                     @elsecan('admin')
                         <h4>Somente do admin pode ver isso.</h4>
                     @endcan
                     
-                    You're logged in!
+                    Você está logado(a)!
                 </div>
             </div>
         </div>
     </div>
 </x-app-layout>
+ 
