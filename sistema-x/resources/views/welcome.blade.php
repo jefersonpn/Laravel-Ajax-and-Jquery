f074345ae3ea4cbe8741d59a3215dc7dbef9909b
Jeferson Nascimento <67881060+jefersonpn@users.noreply.github.com>
2022-06-01 21:14:11 -0400

    Changing welcome msg

--- sistema-x/resources/views/welcome.blade.php
+++ sistema-x/resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
             <div class="header-body text-center mt-7 mb-7">
                 <div class="row justify-content-center">
                     <div class="col-lg-5 col-md-6">
-                        <h1 class="text-white">{{ __('Welcome to Argon Dashboard FREE Laravel Live Preview.') }}</h1>
+                        <h1 class="text-white">{{ __('Seja bem vindo ao sistema de chamada, faça seu login ou cadastre-se já.') }}</h1>
                     </div>
                 </div>
             </div>
